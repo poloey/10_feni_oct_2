f06bac541e8d85705673008fc03ea4d88b67929f
polodev <polodev10@gmail.com>
2017-10-02 14:17:24 +0600

    added class 10 regarding programming concept

---
+++ readme.md
@@ -0,0 +1,33 @@
+# programming 101
+
+* Datatype
+  * Scalar type (who holds only one value)
+    * Numbers
+      * Integer (absolute number - 10)
+      * Floating / Double (Fractional number 13.14)
+    * Boolean (True, False)
+    * String (Text)
+  * Composite type (who holds mix value)
+    * Array names = ['sumon', 'sarwar', 'imran']   //names[1] -> sarwar
+    * Object names = {name1 : 'sumon', name2 : 'sarwar', name3 : 'imran'}  // names['name2'] // names.name2
+* Control flow
+  * iterative
+    for, while, foreach  
+  * conditional
+    if, else, else if, elseif
+* Variable
+  var name1 = 'sumon';
+
+* Function
+  name1 = 'sumon';
+  name1.toUpperCase() = SUMON
+  function add (number1, number2) {
+    return number1 + number2;
+  }
+
+  add(2+3);
+  add(5+3);
+  
+* operator
++ - * / % =
+5 %  2 = 1
---
+++ script.js
@@ -0,0 +1,69 @@
+//something written here wont execute
+//Datatype (scalar)
+var number1 = 10; //integer
+var number2 = 3.1416; //float or double
+var bool1 = true; //boolean
+var bool2 = false; //string
+var string1 = 'Hello Tanim'; //string
+
+//Datatype (composite)
+//array
+var names = ['sumon', 'tanim', 'sarwar'] //array 
+//array index start from 0
+//access array
+names[0] // sumon
+
+//object
+var names = {
+  name1: 'sumon',
+  name2: 'tanim',
+  name3: 'sarwar',
+}
+//accessing object value
+names['name1'] //sumon
+//or
+names.name1 //sumon
+
+
+// control flow
+//conditional control flow
+if (false) {
+  'do something';
+} else if (false) {
+  'do something';
+} else {
+  'do something'
+}
+//iterative control flow
+for (var number = 0; number < 3; number ++ ) {
+  names[number]
+
+
+}
+for (name in names) {
+  name 
+}
+// variable
+var name = 'tanim';
+
+//  function
+var name = 'tanim';
+name.toUpperCase() //TANIM
+
+//user define function 
+function add (n1, n2) {
+  return n1 + n2;
+}
+add(2, 3);
+
+
+//operator
+//+ - * / % = > < == === 
+var number1 = 3;
+var number2 = '3';
+number1 == number2
+number1 === number2
+
+
+
+
