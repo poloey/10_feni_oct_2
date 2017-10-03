c557d8b3529a0de92c1b00848db5c4d28a1c0499
polodev <polodev10@gmail.com>
2017-10-03 17:13:05 +0600

    added new file for oct 3 practice

---
+++ oct_3_practice/loop/script.js
@@ -0,0 +1,20 @@
+// var number = 0;
+// while(number < 11) {
+//   console.log(number)
+//   number = number + 1;
+// }
+
+for (var number = 0; number < 11; number = number + 1) {
+  console.log(number)
+}
+
+// var number = 10;
+
+// while(number > -1) {
+//   console.log(number)
+//   number = number - 1;
+// }
+
+for (var number = 10; number > -1; number = number - 1 ) {
+  console.log(number)
+}
